3f146ee27eea4f76dd23b333ad25c0e64be2bfcc
b0lo0 <ba@dgtl-solutions.de>
2024-10-24 01:03:57 +0100

    Deployment optimisation

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Remove swcMinify if it's causing issues
+  // swcMinify: true,
 };
 
 module.exports = nextConfig;
